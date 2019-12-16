7a086674c282d1793f89de10f2e9b17a79d5ddb5
Grant <gmontgomery1994@gmail.com>
2019-12-15 23:52:33 -0800

    implementing drag and drop from medium article.

--- src/components/Piece/Piece.jsx
+++ src/components/Piece/Piece.jsx
@@ -2,7 +2,77 @@ import React, { Component } from "react";
 import "./Piece.css";
 
 class Piece extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDragging: false,
+      originalX: 0,
+      originalY: 0,
+      translateX: 0,
+      translateY: 0,
+      lastTranslateX: 0,
+      lastTranslateY: 0
+    };
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("mousemove", this.handleMouseMove);
+    window.removeEventListener("mouseup", this.handleMouseUp);
+  }
+
+  handleMouseDown = ({ clientX, clientY }) => {
+    window.addEventListener("mousemove", this.handleMouseMove);
+    window.addEventListener("mouseup", this.handleMouseUp);
+    if (this.props.onDragStart) {
+      this.props.onDragStart();
+    }
+
+    this.setState({ isDragging: true, originalX: clientX, originalY: clientY });
+  };
+
+  handleMouseMove = ({ clientX, clientY }) => {
+    const { isDragging } = this.state;
+    const { onDrag } = this.props;
+
+    if (!isDragging) {
+      return;
+    }
+
+    this.setState(
+      prevState => ({
+        translateX: clientX - prevState.originalX + prevState.lastTranslateX,
+        translateY: clientY - prevState.originalY + prevState.lastTranslateY
+      }),
+      () => {
+        if (onDrag) {
+          onDrag({
+            translateX: this.state.translateX,
+            translateY: this.state.translateY
+          });
+        }
+      }
+    );
+  };
+
+  handleMouseUp = () => {
+    window.removeEventListener("mousemove", this.handleMouseMove);
+    window.removeEventListener("mouseup", this.handleMouseUp);
+
+    this.setState(
+      {
+        originalX: 0,
+        originalY: 0,
+        lastTranslateX: this.state.translateX,
+        lastTranslateY: this.state.translateY,
+        isDragging: false
+      },
+      () => {
+        if (this.props.onDragEnd) {
+          this.props.onDragEnd();
+        }
+      }
+    );
+  };
   render() {
     return <div className="piece-wrapper"></div>;
   }
