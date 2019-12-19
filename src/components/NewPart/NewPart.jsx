abea415ce22904cc722554a1d091ce754840f2dc
Grant <gmontgomery1994@gmail.com>
2019-12-18 22:45:48 -0800

    adding color buttons, react transition group and form for creating new pieces

--- src/components/NewPart/NewPart.jsx
+++ src/components/NewPart/NewPart.jsx
@@ -15,12 +15,68 @@ class NewPart extends Component {
     const { target } = event;
     this.setState({ newPart: target.value });
   };
+
+  changeColor = event => {};
   render() {
     const { newPart } = this.state;
     return (
       <div className="newpart-wrapper">
-        <input type="text" value={newPart} onChange={this.handleUpdate} />
-        <DatePicker></DatePicker>
+        <span>Add something!</span>
+        <br />
+        <form action="">
+          <label htmlFor="">What?</label>
+          <input
+            type="text"
+            name="activity"
+            value={newPart}
+            onChange={this.handleUpdate}
+          />
+          <br />
+          <label htmlFor="" name="time">
+            When?
+          </label>
+          <DatePicker></DatePicker>
+          <br />
+          <label htmlFor="">Color</label>
+          <br />
+          <div className="color-picker-wrapper">
+            <div
+              className="color-picker red"
+              onClick={this.changeColor}
+              name="color"
+              value="rgb()"
+            ></div>
+            <div
+              className="color-picker orange"
+              onClick={this.changeColor}
+              name="color"
+              value="rgb()"
+            ></div>
+            <div
+              className="color-picker green"
+              onClick={this.changeColor}
+              name="color"
+            ></div>
+            <div
+              className="color-picker blue"
+              onClick={this.changeColor}
+              name="color"
+              value="rgb()"
+            ></div>
+            <div
+              className="color-picker purple"
+              onClick={this.changeColor}
+              name="color"
+              value="rgb()"
+            ></div>
+            <div
+              className="color-picker black"
+              onClick={this.changeColor}
+              name="color"
+              value="rgb()"
+            ></div>
+          </div>
+        </form>
       </div>
     );
   }
