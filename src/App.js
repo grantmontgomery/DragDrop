2a41837dcb8fbe3a35f3db7bd94c34f77d13238f
Grant <gmontgomery1994@gmail.com>
2020-01-03 12:55:17 -0800

    setting up hours determination in App component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { AppContext } from "./AppContext";
+import DatePicker from "react-datepicker";
 import { Grid } from "./components";
 import { List } from "./components";
 
@@ -9,9 +10,31 @@ class App extends Component {
     this.state = {
       newPiece: { id: null, value: null, color: null },
       pieces: [],
-      loadApp: false
+      loadApp: false,
+      startDate: "",
+      endDate: ""
     };
   }
+
+  submitDates = () => {};
+
+  handleStartDateChange = (date, event) => {
+    const formatted = new Date(date);
+    this.setState({
+      startDate: formatted,
+      startHour: formatted.getHours(),
+      startMinutes: formatted.getMinutes()
+    });
+  };
+
+  handleEndDateChange = (date, event) => {
+    const formatted = new Date(date);
+    this.setState({
+      endDate: formatted,
+      endHour: formatted.getHours(),
+      endMinutes: formatted.getMinutes()
+    });
+  };
 
   handleSubmit = (event, thestate) => {
     event.preventDefault();
@@ -20,7 +43,7 @@ class App extends Component {
 
   renderFullApp = () => {
     return (
-      <AppContext.Provider>
+      <AppContext.Provider value={this.state}>
         <React.Fragment>
           <List></List>
           <Grid></Grid>
@@ -29,8 +52,38 @@ class App extends Component {
     );
   };
 
+  setHours = () => {
+    return (
+      <div>
+        <span>Picke a date</span>
+        <br />
+        <DatePicker
+          selected={this.state.startDate}
+          name="startDate"
+          onChange={this.handleStartDateChange}
+          autoComplete="off"
+          showTimeSelect
+          dateFormat="Pp"
+        ></DatePicker>
+        <br />
+        <DatePicker
+          name="endDate"
+          selected={this.state.endDate}
+          autoComplete="off"
+          onChange={this.handleEndDateChange}
+          showTimeSelect
+          dateFormat="Pp"
+        ></DatePicker>
+        <br />
+        <button onClick={this.submitDates}>Submit</button>
+      </div>
+    );
+  };
+
   render() {
-    return this.renderFullApp();
+    console.log(this.state);
+    // return this.renderFullApp();
+    return this.setHours();
   }
 }
 
